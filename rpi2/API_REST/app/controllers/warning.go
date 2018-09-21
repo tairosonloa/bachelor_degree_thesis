9ae967784f6f70b6c16d37e3bc2f165805b17a0d
Aitor Alonso <mail@aalonso.pw>
2018-09-21 11:25:10 +0200

    API terminada a falta de test unitarios

--- rpi2/API_REST/app/controllers/warning.go
+++ rpi2/API_REST/app/controllers/warning.go
@@ -13,16 +13,18 @@ func FireAlarm(cpd *models.CPD, alarmSoundPath string) {
 	go soundAlarm(cpd, alarmSoundPath)
 }
 
-// soundAlarm plays an alarm sound six times (~15 seconds) of until
+// soundAlarm plays an alarm sound 4 times (~8 seconds) of until
 // there is not warning (what happens before)
 func soundAlarm(cpd *models.CPD, alarmSoundPath string) {
 	cmdName := "omxplayer"
 	cmdArgs := []string{alarmSoundPath}
-	// TODO loop
-	cmdOut, err := exec.Command(cmdName, cmdArgs...).Output()
-	if err != nil {
-		log.Printf("ERROR warning/soundAlarm(): %v\n", err.Error())
-	} else {
-		log.Printf("Playing sound warning/soundAlarm() %v\n", cmdOut)
+	var cmdOut []byte
+	var err error
+	for i := 0; cpd.IsWarning() && i < 4; i++ {
+		cmdOut, err = exec.Command(cmdName, cmdArgs...).Output()
+		if err != nil {
+			log.Printf("ERROR warning/soundAlarm(): %v\n%v\n", string(cmdOut), err.Error())
+			return
+		}
 	}
 }
