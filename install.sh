f1fba8b19675cf6fa8e6287fe836426da05542e5
Aitor Alonso <aalonso@aalonso.eu>
2019-01-24 10:49:14 +0100

    Fixed some bugs on rpi1 installer

--- install.sh
+++ install.sh
@@ -19,16 +19,28 @@ function set_iptables {
 }
 
 function install_rpi1 {
+    echo "##### Updating hostname and localtime..."
+    echo "rpi1" > /etc/hostname
+    hostname -F /etc/hostname
+    sed -i "s/raspberrypi/rpi1/g" /etc/hosts
+    ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
+
     echo "##### Installing dependences with apt-get..."
     # Install python libs and zabbix agent
     apt-get -y update && apt-get -y upgrade
+    echo iptables-persistent iptables-persistent/autosave_v4 boolean false | debconf-set-selections
+    echo iptables-persistent iptables-persistent/autosave_v6 boolean false | debconf-set-selections
     apt-get -y install python-numpy python3-picamera python3-sense-hat zabbix-agent iptables-persistent
     
     echo "##### Enabling components on rpi..."
     # Enable camera and i2c
-    . raspi-config nonint
-    do_i2c 0
-    do_camera 0
+    raspi-config nonint do_i2c 0    # To use sense hat
+    raspi-config nonint do_camera 0 # To use camera
+
+    echo "##### Configuring permissions..."
+    usermod -aG input lab # To use sense hat
+    usermod -aG i2c lab   # To use sense hat
+    usermod -aG video lab # To use camera
     
     echo "##### Preparing daemons and start on boot..."
     # Copy daemons and enable them
@@ -41,8 +53,16 @@ function install_rpi1 {
     cd -
     
     echo "##### Configuring zabbix..."
-    echo "UserParameter=cpd.temp, /bin/cat /tmp/last_temp.txt" >> /etc/zabbix/zabbix_agentd.conf
-    echo "UserParameter=cpd.hum, /bin/cat /tmp/last_hum.txt" >> /etc/zabbix/zabbix_agentd.conf
+    answ="n"
+    while [ $answ != "y" ] && [ $answ != "Y" ]; do
+        read -p "Indicate zabbix server address (XXX.XXX.XXX.XXX): " addr
+        read -p "Is the IP $addr correct? (Y/n): " answ
+        answ=${answ:-Y}
+    done
+    sed -i "s/Server=127.0.0.1/Server=$addr/g" /etc/zabbix/zabbix_agentd.conf
+    sed -i "s/ServerActive=127.0.0.1/ServerActive=$addr/g" /etc/zabbix/zabbix_agentd.conf
+    sed -i '/# UserParameter=/a UserParameter=cpd.hum, /bin/cat /tmp/last_hum.txt' /etc/zabbix/zabbix_agentd.conf
+    sed -i '/# UserParameter=/a UserParameter=cpd.temp, /bin/cat /tmp/last_temp.txt' /etc/zabbix/zabbix_agentd.conf
     
     echo "##### Setting and saving iptables..."
     set_iptables
@@ -58,7 +78,7 @@ function install_rpi1 {
         read -p "Indicate rpi 2 IP address (XXX.XXX.XXX.XXX): " addr
         read -p "Indicate rpi 2 API port (XXXX): " port
         read -p "Indicate rpi 2 API POST Bearer token (XXXXXXXXXXXX): " token
-        conf='{\n\t"Rpi2APIAddress" : "$addr",\n\t"Rpi2APIPort" : "$port",\n\t"Rpi2APIAuthorizedToken" : "Bearer $token"\n}'
+        conf='{\n\t"Rpi2APIAddress" : "'$addr'",\n\t"Rpi2APIPort" : "'$port'",\n\t"Rpi2APIAuthorizedToken" : "Bearer '$token'"\n}'
         echo "config.json generated:"
         echo -e $conf
         read -p "Is that correct? (Y/n): " answ
@@ -90,9 +110,10 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-echo -e "What rpi are you trying to install?\n\n"
-echo -e "\t(1) rpi1 (inside CPD: monitoring)\n"
-echo -e "\t(2) rpi2 (outside CPD: light control, display, graphs and metrics)\n"
+echo -e "What rpi are you trying to install?\n"
+echo -e "\t(1) rpi1 (inside CPD: monitoring)"
+echo -e "\t(2) rpi2 (outside CPD: light control, display CPD info)"
+echo -e "\t(3) rpi3 (outside CPD: display classrooms info)\n"
 read -p "Please, choose a number. Choose (0) to exit: " opt
 
 case "$opt" in
