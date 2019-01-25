22375db4b678835dab374ae65c3f1a80ff79d249
Aitor Alonso <aalonso@aalonso.eu>
2019-01-25 10:28:43 +0100

    Moved installation path to a system absolute path

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Datacenter monitoring
 
 ## Installation
-1. Clone the git repository inside `/home/lab` (default installation path).
-2. Run `./install.sh` from within and follow the instructions on the terminal.
-3. When installation in finished, the raspberry will automatically reboot.
+1. Create a user who will run the software. In our case `lab`
+2. Clone the repository, `cd` into it, run `./install.sh` and follow the instructions on the terminal.
+3. When installation is finished, the raspberry will automatically reboot.
--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root. Try using \"sudo !!\""
+   exit 1
+fi
+
+if [[ $# -ne 2 ]]; then
+    echo "This script must be called with the user who will run the software as argument."
+    echo -e "\tExample: ./install lab"
+    exit 1
+fi
+INSTALL_USER=$1
+
 function set_iptables {
     # Remove current iptables rules
     iptables -F
@@ -19,31 +31,35 @@ function set_iptables {
 }
 
 function install_rpi1 {
-    echo "##### Updating hostname, localtime and bashrc..."
+    echo -e "\t##### Updating hostname, localtime and bashrc..."
     cp install/.bashrc /root/.bashrc
     echo "rpi1" > /etc/hostname
     hostname -F /etc/hostname
     sed -i "s/raspberrypi/rpi1/g" /etc/hosts
     ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
 
-    echo "##### Installing dependences with apt-get..."
+    echo -e "\t##### Installing dependences with apt-get...\n"
     # Install python libs and zabbix agent
     apt-get -y update && apt-get -y upgrade
     echo iptables-persistent iptables-persistent/autosave_v4 boolean false | debconf-set-selections
     echo iptables-persistent iptables-persistent/autosave_v6 boolean false | debconf-set-selections
     apt-get -y install python-numpy python3-picamera python3-sense-hat zabbix-agent iptables-persistent
     
-    echo "##### Enabling components on rpi..."
+    echo -e "\n\t##### Installing binary files..."
+    cp -r rpi1/scripts /usr/local/bin/rpi1_cpd
+    chown -R $INSTALL_USER:$INSTALL_USER /usr/local/bin/rpi1_cpd
+
+    echo -e "\t##### Enabling components on rpi..."
     # Enable camera and i2c
     raspi-config nonint do_i2c 0    # To use sense hat
     raspi-config nonint do_camera 0 # To use camera
 
-    echo "##### Configuring permissions..."
-    usermod -aG input lab # To use sense hat
-    usermod -aG i2c lab   # To use sense hat
-    usermod -aG video lab # To use camera
+    echo -e "\t##### Configuring permissions..."
+    usermod -aG input $INSTALL_USER # To use sense hat
+    usermod -aG i2c $INSTALL_USER   # To use sense hat
+    usermod -aG video $INSTALL_USER # To use camera
     
-    echo "##### Preparing daemons and start on boot..."
+    echo -e "\t##### Preparing daemons and start on boot...\n"
     # Copy daemons and enable them
     cp install/rpi1/daemons/* /etc/systemd/system
     cd /etc/systemd/system
@@ -53,7 +69,7 @@ function install_rpi1 {
     systemctl enable zabbix-agent
     cd -
     
-    echo "##### Configuring zabbix..."
+    echo -e "\n\t##### Configuring zabbix...\n"
     answ="n"
     while [ $answ != "y" ] && [ $answ != "Y" ]; do
         read -p "Indicate zabbix server address (XXX.XXX.XXX.XXX): " addr
@@ -65,7 +81,7 @@ function install_rpi1 {
     sed -i '/# UserParameter=/a UserParameter=cpd.hum, /bin/cat /tmp/last_hum.txt' /etc/zabbix/zabbix_agentd.conf
     sed -i '/# UserParameter=/a UserParameter=cpd.temp, /bin/cat /tmp/last_temp.txt' /etc/zabbix/zabbix_agentd.conf
     
-    echo "##### Setting and saving iptables..."
+    echo -e "\n\t##### Setting and saving iptables..."
     set_iptables
     # Allow zabbix in our network
     iptables -A INPUT -p tcp --dport 10050 -s 163.117.170.0/24 -j ACCEPT
@@ -73,7 +89,7 @@ function install_rpi1 {
     # Save rules so they persist after reboot
     iptables-save > /etc/iptables/rules.v4
     
-    echo "##### Generating config.json..."
+    echo -e "\t##### Generating /etc/cpd_rpi1_conf.json...\n"
     answ="n"
     while [ $answ != "y" ] && [ $answ != "Y" ]; do
         read -p "Indicate rpi 2 IP address (XXX.XXX.XXX.XXX): " addr
@@ -85,31 +101,37 @@ function install_rpi1 {
         read -p "Is that correct? (Y/n): " answ
         answ=${answ:-Y}
     done
-    echo -e $conf > config.json
+    echo -e $conf > /etc/cpd_rpi1_conf.json
+    chown 600 /etc/cpd_rpi1_conf.json
+    chown $INSTALL_USER:$INSTALL_USER /etc/cpd_rpi1_conf.json
 }
 
 function install_rpi2 {
-    echo "##### Copying files..."
-    # Copy binary and alarm.mp3
-    echo "##### Preparing daemons and start on boot..."
+    echo -e "\t##### Updating hostname, localtime and bashrc..."
+    cp install/.bashrc /root/.bashrc
+    echo "rpi2" > /etc/hostname
+    hostname -F /etc/hostname
+    sed -i "s/raspberrypi/rpi2/g" /etc/hosts
+    ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
+
+    echo -e "\t##### Installing binary files..."
+    cp install/rpi2/rpi2_api_arm /usr/local/bin/rpi2_api_arm # Rpi2 API binary
+    cp install/rpi2/alarm.mp3 /usr/local/share/alarm.mp3     # Rpi2 API alarm sound file
+
+    echo -e "\t##### Preparing daemons and start on boot..."
     # Copy daemons and enable it
-    echo "##### Setting and enabling iptables..."
+    echo -e "\t##### Setting and enabling iptables..."
     # Set and enable iptables
-    echo "##### Generating config.json..."
+    echo -e "\t##### Generating config.json..."
     #create_config_json()
-    read -p "##### WARNING: Please, press the philips hue bridge button before continue. Press enter when pressed."
-    echo "##### Pairing philips hue bridge..."
+    read -p "\t##### WARNING: Please, press the philips hue bridge button before continue. Press enter when pressed."
+    echo -e "\t##### Pairing philips hue bridge..."
 }
 
 function install_rpi3 {
     echo "Error: No implemented yet."
     exit 1
 }
-
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root"
-   exit 1
-fi
 
 echo -e "What rpi are you trying to install?\n"
 echo -e "\t(1) rpi1 (inside CPD: monitoring)"
