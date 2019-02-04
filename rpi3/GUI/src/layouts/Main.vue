3880d68adbe0e9ffd4719a79d81cc95ae3b0ebf8
Aitor Alonso <aalonso@aalonso.eu>
2019-02-04 12:32:30 +0100

    Basic layout finished

---
+++ rpi3/GUI/src/layouts/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrapper">
+    <header class="header">
+      Laboratorio de Departamento de Informática
+    </header>
+    <article class="article">
+        <slot />
+    </article>
+    <aside class="aside">
+        <slot name="reservations" />
+    </aside>
+    <footer class="footer">
+        <slot name="titulars" />
+    </footer>
+  </div>
+</template>
--- rpi3/GUI/src/pages/Index.vue
+++ rpi3/GUI/src/pages/Index.vue
@@ -1,31 +1,15 @@
 <template>
   <Layout>
     
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-    
-    <h1>Hello, world!</h1>
-   
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+    Aquí va el vídeo
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
+    <template slot="reservations">
+      Aquí van las reservas
+    </template>
+
+    <template slot="titulars">
+      Aquí van los titulares
+    </template>
 
   </Layout>
 </template>
-
-<script>
-export default {
- // ..
-}
-</script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
