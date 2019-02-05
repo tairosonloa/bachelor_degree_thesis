19a976d8b1911f42e726bc1215b6ed0ab67b2346
Aitor Alonso <aalonso@aalonso.eu>
2019-02-05 11:26:55 +0100

    Gatsby basic layout

--- rpi3/GUI/src/pages/index.js
+++ rpi3/GUI/src/pages/index.js
@@ -1,3 +1,25 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div className="wrapper">
+    <header className="header">
+      <div className="headerTitle">
+        {/* TODO: add logo */}
+        Laboratorio de Departamento de Informática
+      </div>
+      <div className="datetime">
+        {/* <Clock/>
+        <Date/> */}
+      </div>
+    </header>
+    <article className="article">
+
+    </article>
+    <aside className="aside">
+    
+    </aside>
+    <footer className="footer">
+
+    </footer>
+  </div>
+)
