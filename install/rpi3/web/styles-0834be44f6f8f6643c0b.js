9c2ffa55f766ef4af6ace5c7390fe43f8d8349dd
Aitor Alonso <aalonso@aalonso.eu>
2019-02-27 14:02:06 +0100

    New colors

---
+++ install/rpi3/web/styles-0834be44f6f8f6643c0b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{105:function(o,c,e){},141:function(o,c,e){o.exports={clock:"clock-module--clock--3msBr",date:"clock-module--date--2Ndip",time:"clock-module--time--2rZDb"}},142:function(o,c,e){o.exports={card:"cards-module--card--1m33Q",endCard:"cards-module--endCard--_AYSX",classroom:"cards-module--classroom--qBn8O"}},143:function(o,c,e){o.exports={marquee:"information-module--marquee--3Tiv7"}}}]);
+//# sourceMappingURL=styles-0834be44f6f8f6643c0b.js.map
